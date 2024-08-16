80badc40165a3ed1c957aa6c060d92ca9d2e73cf
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-08-16 22:25:50 +0530

    added secure and same prop to cookie response

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,12 +1,19 @@
 export const genrateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWebToken()
-    const { password:pas, ...rest} = user._doc
-    res.status(statusCode).cookie("token", token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        httpOnly : true
-    }).json({
-        success: true,
-        message,
-        data:rest
+  const token = user.generateJsonWebToken()
+  const { password: pas, ...rest } = user._doc
+  res
+    .status(statusCode)
+    .cookie('token', token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
     })
-} 
+    .json({
+      success: true,
+      message,
+      data: rest,
+    })
+}
