f5388520f56dd5fc10820148b739c6de73b7b2a5
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-09-09 19:16:54 +0530

    updated

--- portfolio-frontend/src/pages/subpages/Apps.jsx
+++ portfolio-frontend/src/pages/subpages/Apps.jsx
@@ -21,7 +21,7 @@ function Apps() {
   console.log(apps);
   return (
     <div className="w-full flex flex-col gap-8 sm:gap-12">
-      <h1 className="text-tubeLight-effect text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] tracking-[15px] dancing_text mx-auto w-fit">
+      {/* <h1 className="text-tubeLight-effect text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] tracking-[15px] dancing_text mx-auto w-fit">
         My Apps
       </h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
@@ -39,7 +39,7 @@ function Apps() {
               <p className="text-muted-foreground text-center">{app.name}</p>
             </Card>
           ))}
-      </div>
+      </div> */}
     </div>
   );
 }
