cf230afabc3a42966f961313f56bfbd3e75e1b78
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-10-01 01:09:10 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,7 @@ dotenv.config()
 
 app.use(
   cors({
-    origin: [
-      process.env.PORTFOLIO_URL,
-      process.env.DASHBOARD_URL,
-      process.emv.UPTIME_TOGGLER,
-    ],
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
   })
